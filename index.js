0f2200c6c4f00b396ded87d2130fc2b95bb93889
vanagila <vanagilakedna@gmail.com>
2025-07-19 17:03:59 -0300

    build: initial configs

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`)
+})
+
+app.get("/", (_, res) => res.status(200).json({ ok: true }))
